bb3d401b4684276605ce94fc28c66217a0664b7c
warlynramirez@hotmail.com <warlynramirez@hotmail.com>
2023-02-24 10:58:43 -0500

    Student Profile V1

---
+++ BoJo/Models/StudentProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace BoJo.Models
+{
+    public class StudentProfile
+    {
+        public String ProgramType { get; set; }
+        public String Major { get; set; }
+        public String Location { get; set; }
+        public String School_Size { get; set; }
+        public int ACT_Score { get; set; }
+        public int SAT_Score { get; set; }
+        public float GPA { get; set; }
+        public float GPA_outof { get; set; }
+
+    }
+    public class Major
+    {
+        public String Id { get; set; }
+        public string Title { get; set; }
+    }
+}
