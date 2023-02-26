4c0cc47182d9a030803752515727fcb5d35a8290
eddiejdavid <edavid1385@gmail.com>
2023-02-25 21:23:03 -0500

    work in progress of customer support

--- BoJo/wwwroot/js/site.js
+++ BoJo/wwwroot/js/site.js
@@ -1,5 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿open = false;
 
-// Write your JavaScript code.
+$("#speechBubbleImage").click(function () {
+    if (open == false)
+    {
+        $('.transform').toggleClass('openChatbox');
+        document.getElementById("messageBox").style.display = 'block';
+        document.getElementById("sendButton").style.display = 'block';
+        document.getElementById("closeButton").style.display = 'block';
+        document.getElementById("speechBubbleImage").style.display = 'none';
+        open = true;
+    }
+});
 
+$("#closeButton").click(function () {
+    if (open == true) {
+        $('.transform').toggleClass('openChatbox');
+        document.getElementById("messageBox").style.display = 'none';
+        document.getElementById("sendButton").style.display = 'none';
+        document.getElementById("closeButton").style.display = 'none';
+        document.getElementById("speechBubbleImage").style.display = 'block';
+        open = false;
+    }
+});
